d3de66cffff3ad0ae9766a397806f757ec6e1819
nVoid Art-Tech Limited <hello@nvoid.com>
2015-03-23 05:32:27 -0400

    email submit instead of db 29

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def submit():
             # send job back to email for reference
             msg = Message('Job posting attached', sender="spacejobs.us@gmail.com", recipients=["spacejobs.us@gmail.com"])
             msg.body = str(term) + '\n' + str(location) + '\n' + str(jobposition) + '\n' + str(department) + '\n' + str(agency) + '\n' + str(dateposted) + '\n' + str(link)
-            #mail.send(msg)
+            mail.send(msg)
 
             # flash message as well
             #return redirect(url_for('main'))
