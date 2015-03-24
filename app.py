f869ac972338bfc5ceb5ba4e3c99d8cd52235295
nVoid Art-Tech Limited <hello@nvoid.com>
2015-03-23 23:32:11 -0400

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'spacejobs.us@gmail.com'
-app.config['MAIL_PASSWORD'] = 'priA-glOl-R'
+app.config['MAIL_PASSWORD'] = os.environ['EMAIL_PASS']
 mail = Mail(app)
 
 # config and setup db
